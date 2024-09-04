568d58fe44a2fcbbfa77bb58476d8e77416e15b9
NatalieDillon <112569645+NatalieDillon@users.noreply.github.com>
2024-09-04 13:53:33 +0100

    Q1 and Q2 completed

--- CSharpSelection/Program.cs
+++ CSharpSelection/Program.cs
@@ -6,62 +6,23 @@ namespace CSharpSelection
 	{
 		static void Main()
 		{
-			Console.WriteLine("# Assign Letter Grade");
-			string grade = SelectionExamples.AssignLetterGrade(90);
-			Console.WriteLine($"Your grade was a {grade}\n");
-			//// Can use Debug.Assert to check return values
-			Debug.Assert(SelectionExamples.AssignLetterGrade(70) == "Merit", "Merit expected");
+			Console.WriteLine("Q1:");
+			Console.WriteLine($"FizzBuzz(4) returns {SelectionExercises.FizzBuzz(4)}");
+			Console.WriteLine($"FizzBuzz(3) returns {SelectionExercises.FizzBuzz(3)}");
+			Console.WriteLine($"FizzBuzz(5) returns {SelectionExercises.FizzBuzz(5)}");
+			Console.WriteLine($"FizzBuzz(15) returns {SelectionExercises.FizzBuzz(15)}");
+			Console.WriteLine($"FizzBuzzAlt(4) returns {SelectionExercises.FizzBuzzAlt(4)}");
+			Console.WriteLine($"FizzBuzzAlt(3) returns {SelectionExercises.FizzBuzzAlt(3)}");
+			Console.WriteLine($"FizzBuzzAlt(5) returns {SelectionExercises.FizzBuzzAlt(5)}");
+			Console.WriteLine($"FizzBuzzAlt(15) returns {SelectionExercises.FizzBuzzAlt(15)}");
+			Console.WriteLine();
 
-			//Console.WriteLine("# Conditional Operations");
-			//SelectionExamples.ConditionalOperations(false, true);
-			//Console.WriteLine();
-
-			//Console.WriteLine("# Check for valid Input");
-			//InputNumber();
-			//Console.WriteLine();
-
-			//Console.WriteLine("# Lift Takeoff");
-			///string message = SelectionExamples.LiftTakeoff(5, 142d);
-			//Console.WriteLine($"{message}\n");
-
-			//Console.WriteLine("# Determine tip");
-			//decimal tip = SelectionExamples.DetermineTip("Good");
-			//Console.WriteLine($"The tip will be {tip}\n");
-
-			//Console.WriteLine("# Determine tip with enum");
-			//decimal tip2 = SelectionExamples.DetermineTip(SelectionExamples.ServiceLevel.Good);
-			//Console.WriteLine($"The tip will be {tip2}\n");
-
-			//Console.WriteLine("# Generating a random number");
-			//RandomExample();
-		}
-
-		static void RandomExample()
-		{
-			Random random = new();
-			int num = random.Next(10);
-			Console.WriteLine($"Your random number is {num}\n");
-		}
-
-		static void InputNumber()
-		{
-			Console.WriteLine("Please enter a number between one and fifty");
-			string? strNum = Console.ReadLine();
-			if (int.TryParse(strNum, out int val))
-			{
-				if (val > 0 && val < 51)
-				{
-					Console.WriteLine("Your number is valid");
-				}
-				else
-				{
-					Console.WriteLine("Your number must be between one and fifty");
-				}
-			}
-			else
-			{
-				Console.WriteLine("You did not enter a valid number");
-			}			
+			Console.WriteLine("Q2:");
+			Console.WriteLine($"a is a vowel: {SelectionExercises.IsVowel('A')}");
+			Console.WriteLine($"z is a vowel: {SelectionExercises.IsVowel('z')}");
+			Console.WriteLine($"i is a vowel: {SelectionExercises.IsVowel('i')}");
+			Console.WriteLine($"? is a vowel: {SelectionExercises.IsVowel('?')}");
+			Console.WriteLine();
 		}
 	}
 }
---
+++ CSharpSelection/SelectionExercises.cs
@@ -0,0 +1,61 @@
+﻿
+
+using System.Linq;
+
+namespace CSharpSelection
+{
+	public static class SelectionExercises
+	{
+		// Q1
+		public static string FizzBuzz(int number)
+		{
+			string result = Convert.ToString(number);
+			if ((number % 5 == 0) && (number % 3 == 0))
+			{
+				result = "FizzBuzz";
+			}
+			else if (number % 5 == 0)
+			{
+				result = "Buzz";
+			}
+			else if (number % 3 == 0)
+			{
+				result = "Fizz";
+			}
+			return result;
+		}
+
+		public static string FizzBuzzAlt(int number)
+		{
+			string result = string.Empty;
+			if (number % 3 == 0)
+			{
+				result = "Fizz";
+			}
+			if (number % 5 == 0)
+			{
+				result += "Buzz";
+			}
+			return result == string.Empty ? Convert.ToString(number) : result;
+		}
+
+		// Q2
+		public static bool IsVowel(char letter)
+		{
+			bool result = false;
+			letter = Char.ToLower(letter);
+			switch (letter)
+			{
+				case ('a'):
+				case ('e'):
+				case ('i'):
+				case ('o'):
+				case ('u'):
+					result = true;
+					break;
+			}
+			return result;
+		}
+
+	}
+}
